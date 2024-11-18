3fc41a89b23be7fb08eb911026e175a90f64c950
Megan Goodman <meganngoodman@gmail.com>
2024-11-18 22:08:29 +0000

    first commit

--- index.js
+++ index.js
@@ -18,13 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+    if (!req.params.date || req.params.date.trim() === "") {
+      const dateTime =  new Date();
+      const unix = dateTime.getTime();
+      const utc = dateTime.toUTCString();
+      return res.json({unix, utc});
+    }
+
+    let dateTime
+    let dateString
+    
+    dateString = req.params.date
+    if (isNaN(dateString) == true) {
+      dateTime = new Date(dateString);
+    } else {
+      dateTime = new Date(+dateString);
+    }
+
+    if (dateTime.toString() === "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    } 
+
+    const unix = dateTime.getTime();
+    const utc = dateTime.toUTCString();
+    return res.json({unix, utc});
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
